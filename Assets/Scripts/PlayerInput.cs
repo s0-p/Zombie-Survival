f30fac83cfb0d26e89de71e1601a7ba4de2f7496
s0 <b00v0429@gmail.com>
2023-10-11 12:56:23 +0900

    feat : add player movement
    플레이어 이동 구현

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    public string _moveAxisName = "Vertical";
+    public string _rotateAxisName = "Horizontal";
+    public string _fireButtonName = "Fire1";
+    public string _reloadButtonName = "Reload";
+
+    public float Move { get; private set; }
+    public float Rotate { get; private set; }
+    public bool Fire { get; private set; }
+    public bool Reload { get; private set; }
+
+    void Update()
+    {
+        Move = Input.GetAxis(_moveAxisName);
+        Rotate = Input.GetAxis(_rotateAxisName);
+        Debug.Log("Move : " + Move);
+        Debug.Log("Rotate : " + Rotate);
+
+        Fire = Input.GetButton(_fireButtonName);
+        Reload = Input.GetButtonDown(_reloadButtonName);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float _moveSpeed;
+    public float _rotSpeed;
+
+    PlayerInput _playerInput;
+    Rigidbody _rigidBody;
+    Animator _animator;
+
+    void Awake()
+    {
+        _playerInput = GetComponent<PlayerInput>();
+        _rigidBody = GetComponent<Rigidbody>();
+        _animator = GetComponent<Animator>();
+    }
+    void FixedUpdate()
+    {
+        Rotate();
+        Move();
+        _animator.SetFloat("Speed", _playerInput.Move);
+    }
+    void Move()
+    {
+        Vector3 moveDist = _playerInput.Move * transform.forward * _moveSpeed * Time.deltaTime;
+        _rigidBody.MovePosition(_rigidBody.position + moveDist);
+    }
+    void Rotate()
+    {
+        float turn = _playerInput.Rotate * _rotSpeed * Time.deltaTime;
+        _rigidBody.rotation = _rigidBody.rotation * Quaternion.Euler(0, turn, 0);
+        Debug.Log(_rigidBody.rotation.ToString());
+    }
+}
