5bc3287a6f8fabb303c9499e6962fe005d637cd8
s0 <b00v0429@gmail.com>
2023-10-11 16:19:23 +0900

    feat : add gun

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,116 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public enum STATE
+    {
+        READY, 
+        EMPTY,
+        RELOADING
+    }
+    public STATE CurState { get; private set; }
+    //-------------------------------------------
+    public float _damage = 25f;
+    float _fireDist = 50;
+    //-------------------------------------------
+    public int _ammoRemain = 100;
+    public int _magCapacity = 25;
+    public int _magAmo;
+    //-------------------------------------------
+    public float _timeBetfire = 0.12f;
+    public float _reloadTime = 1.8f;
+    float _lastFireTime;
+    //-------------------------------------------
+    public Transform _trsfFire;
+    //-------------------------------------------
+    public ParticleSystem _muzzleFlashEffect;
+    public ParticleSystem _shellEjectEffect;
+    //-------------------------------------------
+    LineRenderer _bulletLineRenderer;
+    //-------------------------------------------
+    AudioSource _gunAudioPlayer;
+    public AudioClip _shotClip;
+    public AudioClip _reloadClip;
+    //--------------------------------------------------------------------
+    void Awake()
+    {
+        _gunAudioPlayer = GetComponent<AudioSource>();
+        _bulletLineRenderer = GetComponent<LineRenderer>();
+        _bulletLineRenderer.positionCount = 2;
+        _bulletLineRenderer.enabled = false;
+    }
+    void OnEnable()
+    {
+        _magAmo = _magCapacity;
+        CurState = STATE.READY;
+        _lastFireTime = 0f;
+    }
+    //--------------------------------------------------------------------
+    public void Fire()
+    {
+        if (CurState == STATE.READY && Time.time >= _lastFireTime + _timeBetfire)
+        {
+            _lastFireTime = Time.time;
+            Shot();
+        }
+    }
+    void Shot()
+    {
+        Vector3 hitPos = Vector3.zero;
+        if (Physics.Raycast(_trsfFire.position, _trsfFire.forward, out RaycastHit hit, _fireDist))
+        {
+            IDamageable target = hit.collider.GetComponent<IDamageable>();
+            if (target != null)
+            {
+                target.OnDamage(_damage, hit.point, hit.normal);
+            }
+            hitPos = hit.point;
+        }
+        else
+            hitPos = _trsfFire.position + _trsfFire.forward * _fireDist;
+        
+        StartCoroutine(Crt_ShotEffect(hitPos));
+        --_magAmo;
+
+        if (_magAmo <= 0)
+            CurState = STATE.EMPTY;
+    }
+    IEnumerator Crt_ShotEffect(Vector3 hitPos)
+    {
+        _muzzleFlashEffect.Play();
+        _shellEjectEffect.Play();
+
+        _gunAudioPlayer.PlayOneShot(_shotClip);
+        _bulletLineRenderer.SetPosition(0, _trsfFire.position);
+        _bulletLineRenderer.SetPosition(1, hitPos);
+        _bulletLineRenderer.enabled = true;
+        yield return new WaitForSeconds(0.03f);
+        _bulletLineRenderer.enabled = false;
+    }
+    //--------------------------------------------------------------------
+    public bool Reload()
+    {
+        if (CurState == STATE.RELOADING || _ammoRemain <= 0 || _magAmo >= _magCapacity)
+            return false;
+
+        StartCoroutine(Crt_ReloadRoutine());
+        return true;
+    }
+    IEnumerator Crt_ReloadRoutine()
+    {
+        CurState = STATE.RELOADING;
+        _gunAudioPlayer.PlayOneShot(_reloadClip);
+        yield return new WaitForSeconds(_reloadTime);
+
+        int ammoToFill = _magCapacity - _magAmo;
+        if (_ammoRemain < ammoToFill)
+            ammoToFill = _ammoRemain;
+
+        _magAmo += ammoToFill;
+        _ammoRemain -= ammoToFill;
+
+        CurState = STATE.READY;
+    }
+}
---
+++ Assets/Scripts/Player/PlatyerShooter.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatyerShooter : MonoBehaviour
+{
+    PlayerInput _playerInput;
+    Animator _animator;
+    //---------------------------------
+    public Gun _gun;
+    //---------------------------------
+    public Transform _trsfGunPivot;
+    public Transform _trsfLHandMount;
+    public Transform _trsfRHandMount;
+    //--------------------------------------------------------------------
+    void Awake()
+    {
+        _playerInput = GetComponent<PlayerInput>();
+        _animator = GetComponent<Animator>();
+    }
+    void OnEnable() { _gun.gameObject.SetActive(true); }
+    void OnDisable() { _gun.gameObject.SetActive(false); }
+    void Update()
+    {
+        if (_playerInput.Fire)
+            _gun.Fire();
+
+        else if (_playerInput.Reload)
+        {
+            if (_gun.Reload())
+                _animator.SetTrigger("Reload");
+        }
+    }
+    void OnAnimatorIK(int layerIndex)
+    {
+        _trsfGunPivot.position = _animator.GetIKHintPosition(AvatarIKHint.RightElbow);
+
+        _animator.SetIKPositionWeight(AvatarIKGoal.LeftHand, 1f);
+        _animator.SetIKRotationWeight(AvatarIKGoal.LeftHand, 1f);
+        _animator.SetIKPosition(AvatarIKGoal.LeftHand, _trsfLHandMount.position);
+        _animator.SetIKRotation(AvatarIKGoal.LeftHand, _trsfLHandMount.rotation);
+
+        _animator.SetIKPositionWeight(AvatarIKGoal.RightHand, 1f);
+        _animator.SetIKRotationWeight(AvatarIKGoal.RightHand, 1f);
+        _animator.SetIKPosition(AvatarIKGoal.RightHand, _trsfRHandMount.position);
+        _animator.SetIKRotation(AvatarIKGoal.RightHand, _trsfRHandMount.rotation);
+    }
+}
